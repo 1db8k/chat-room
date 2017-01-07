37090aca72ccaa198d1ab039e5d13fe4d6cddc01
Your Name <you@example.com>
2017-01-07 17:05:32 +0100

    changed format of routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,26 +6,23 @@ router.get(['/', '/login'], (req, res, next) => {
     title: 'Login'
   })
 })
-
-router.get('/rooms', (req, res, next) => {
-  res.render('rooms', {
-    cssPath: 'roomsStyles',
-    title: 'Rooms'
+  .get('/rooms', (req, res, next) => {
+    res.render('rooms', {
+      cssPath: 'roomsStyles',
+      title: 'Rooms'
+    })
   })
-})
-
-router.get('/chatroom', (req, res, next) => {
-  res.render('chatroom', {
-    cssPath: 'chatroomStyles',
-    title: 'Chatroom'
+  .get('/chatroom', (req, res, next) => {
+    res.render('chatroom', {
+      cssPath: 'chatroomStyles',
+      title: 'Chatroom'
+    })
   })
-})
-
-router.get('*', (req, res, next) => {
-  res.render('404', {
-    cssPath: '404',
-    title: 'Error 404'
+  .get('*', (req, res, next) => {
+    res.render('404', {
+      cssPath: '404',
+      title: 'Error 404'
+    })
   })
-})
 
 module.exports = router
