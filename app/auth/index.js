621d6bf05e64fe69fb1b81a6c42c35fa1392e0c9
Your Name <you@example.com>
2017-01-07 16:48:01 +0100

    added serialize and deserialize to passport

--- app/auth/index.js
+++ app/auth/index.js
@@ -4,6 +4,16 @@ const config = require('../config')
 const FacebookStrategy = require('passport-facebook').Strategy
 
 module.exports = function authMiddleWare () {
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+
+  passport.deserializeUser(function (id, done) {
+    hlpr.findUserByMongoID(id)
+        .then(user => done(null, user))
+        .catch(err => console.err(err))
+  })
+
   function authCallback (accessToken, refreshToken, profile, done) {
     // Find a user in the local db using profile.id
     hlpr.findUser(profile.id)
