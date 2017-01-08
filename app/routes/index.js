4fe95ddb0b1399d84617917d5e094109c06bc15a
Your Name <you@example.com>
2017-01-08 01:29:10 +0100

    added redirectIfNotLoggedIn middleware to relevant routes and twitter authentication routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const hlprs = require('../helpers')
 const passport = require('passport')
 
 router.get(['/', '/login'], (req, res, next) => {
@@ -7,24 +8,29 @@ router.get(['/', '/login'], (req, res, next) => {
     title: 'Login'
   })
 })
-  .get('/rooms', (req, res, next) => {
+  .get('/rooms', hlprs.midWare.redirectIfNotLoggedIn, (req, res, next) => {
     res.render('rooms', {
       cssPath: 'roomsStyles',
       title: 'Rooms',
       user: req.user
     })
   })
-  .get('/chatroom', (req, res, next) => {
+  .get('/chatroom', hlprs.midWare.redirectIfNotLoggedIn, (req, res, next) => {
     res.render('chatroom', {
       cssPath: 'chatroomStyles',
-      title: 'Chatroom'
+      title: 'Chatroom',
+      user: req.user
     })
   })
   .get('/auth/facebook', passport.authenticate('facebook'))
-  .get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/rooms',
-    failureRedirect: '/',
-    failureFlash: true
-  }))
+  .get('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/rooms', failureRedirect: '/', failureFlash: true }))
+
+  .get('/auth/twitter', passport.authenticate('twitter'))
+  .get('/auth/twitter/callback', passport.authenticate('twitter', { successRedirect: '/rooms', failureRedirect: '/', failureFlash: true }))
+  .get('/logout', function (req, res) {
+    req.logout()
+    res.redirect('/')
+  })
   .get('*', (req, res, next) => {
     res.render('404', {
       cssPath: '404',
