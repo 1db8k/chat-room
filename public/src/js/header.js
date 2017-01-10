534916f1c9395ce4f715f3e4a67b816fc2a42b8e
Your Name <you@example.com>
2017-01-10 15:53:32 +0100

    dropdown header logic

--- public/src/js/header.js
+++ public/src/js/header.js
@@ -1,11 +1,28 @@
 var menuSVG = document.querySelectorAll('.menuSVG')[0]
 var dropdownMenu = document.querySelectorAll('.dropdownMenu')[0]
+var wrapper = document.createElement('div')
+wrapper.class = 'wrapper'
+wrapper.style.position = 'absolute'
+wrapper.style.width = '100vw'
+wrapper.style.top = '0'
+wrapper.style.left = '0'
+wrapper.style.height = '100vh'
 
-menuSVG.addEventListener('click', function () {
-  dropdownMenu.display = 'inherit'
-  dropdownMenu.focus()
-})
+if (menuSVG) {
+  menuSVG.addEventListener('click', function (event) {
+    var parent = dropdownMenu.parentNode
 
-dropdownMenu.on('blur', function () {
-  dropdownMenu.display = 'none'
-})
+    parent.replaceChild(wrapper, dropdownMenu)
+    wrapper.appendChild(dropdownMenu)
+    dropdownMenu.style.display = 'flex'
+    wrapper.style.display = 'initial'
+  })
+
+  wrapper.addEventListener('click', function (event) {
+    var grandParent = dropdownMenu.parentNode.parentNode
+
+    grandParent.replaceChild(dropdownMenu, wrapper)
+    dropdownMenu.style.display = 'none'
+    wrapper.style.display = 'none'
+  })
+}
