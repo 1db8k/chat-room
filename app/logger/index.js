3e4754c6abffba73efd3e3d2b725ab548bdc6004
Your Name <you@example.com>
2017-01-13 11:14:02 +0100

    resolved bug: not updating users list on sign out

--- app/logger/index.js
+++ app/logger/index.js
@@ -2,7 +2,7 @@ const winston = require('winston')
 module.exports = new winston.Logger({
   transports: [
     // new winston.transports.File({ level: 'debug', filename: './chatroom.log', handleExceptions: true }),
-    new winston.transports.Console({ level: 'error', json: true, handleExceptions: true })
+    new winston.transports.Console({ level: 'debug', json: true, handleExceptions: true })
   ],
   exitOnError: false
 })
